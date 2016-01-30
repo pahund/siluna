b6f949d3150d0c9d20e13d99f941b438e3f19a49
Patrick Hund <pahund@team.mobile.de>
2016-01-30 11:05:56 +0100

    Redux running in production mode when deployed
    Change-Id: I7c791644685a0c72309a1acf5599ca7112de51a3

--- webpack.config.js
+++ webpack.config.js
@@ -32,6 +32,11 @@ module.exports = {
             compress: {
                 warnings: false
             }
+        }),
+        
+        // make Redux run in production mode
+        new webpack.DefinePlugin({
+            "process.env.NODE_ENV": "\"production\""
         })
     ]
 };
