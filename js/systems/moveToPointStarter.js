ed62bb1fff584ef9b669da04ea5be33d33a1aebd
Patrick Hund <pahund@team.mobile.de>
2016-02-29 19:22:33 +0100

    work in progress: lerp movement, changing direction
    Change-Id: I1d87b8967de396396eed899a979b32e0ee286ead

--- js/systems/moveToPointStarter.js
+++ js/systems/moveToPointStarter.js
@@ -5,5 +5,21 @@
  * @since 24 Feb 2016
  */
 import movesWithAcceleration from "../components/movesWithAcceleration";
+import { MOVES_WITH_ACCELERATION } from "../components";
 
-export default (prevEntity, target, targetSpeed, lerpSpeed, callback) => prevEntity.update(movesWithAcceleration(target, targetSpeed, lerpSpeed, callback));
+export default (prevEntity, target, targetSpeed, lerpSpeed, callback) => {
+    let prevComponent = prevEntity.get(MOVES_WITH_ACCELERATION),
+        nextComponent;
+    if (!prevComponent) {
+        nextComponent = movesWithAcceleration(target, targetSpeed, lerpSpeed, callback)
+    } else {
+        nextComponent = {
+            ...prevComponent,
+            target,
+            targetSpeed,
+            lerpSpeed,
+            velocity: null
+        };
+    }
+    return prevEntity.update(nextComponent);
+}
