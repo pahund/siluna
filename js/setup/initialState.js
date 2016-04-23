72f2e40b434e4d9fb9b9660827f63575f7e06d08
Patrick Hund <pahund@team.mobile.de>
2016-04-23 15:14:26 +0200

    dragging enabled once again
    Change-Id: Ib33a4584cfa1859614d595fced6d7d0d9e92faa9

--- js/setup/initialState.js
+++ js/setup/initialState.js
@@ -12,18 +12,18 @@ import hasAnimation from "../components/hasAnimation";
 import hasSpine from "../components/hasSpine";
 import hasDebugDots from "../components/hasDebugDots";
 import {
+    CHANGE_DIRECTION,
     TAP_ON_SCREEN,
-    //TOUCH_START_ON_SCREEN,
-    //TOUCH_MOVE_ON_SCREEN,
-    //TOUCH_END_ON_SCREEN,
+    TOUCH_START_ON_SCREEN,
+    TOUCH_MOVE_ON_SCREEN,
+    TOUCH_END_ON_SCREEN,
     CURRENT_TAP,
     ANIMATE,
     ROTATE_TO_POINT,
     ROTATE_TO_VECTOR,
-    MOVE_TO_POINT
-    //START_MOVING_TO_POINT,
-    //STOP_MOVING_TO_POINT,
-    //DEBUG
+    MOVE_TO_POINT,
+    START_MOVING_TO_POINT,
+    STOP_MOVING_TO_POINT
 } from "../actions";
 import config from "../config";
 import Point from "../math/Point";
@@ -44,26 +44,19 @@ export default {
                 new Action(MOVE_TO_POINT, "siluna", CURRENT_TAP, config.speed.movement, EASE_IN_OUT_SINE),
                 new Action(ROTATE_TO_VECTOR, "siluna", new Vector(0, -1), config.speed.rotation / 3),
                 new Action(ANIMATE, "siluna", "treading-water")
-                //new Action(DEBUG, "tap on screen sequence done")
             )
+        ),
+        [TOUCH_START_ON_SCREEN]: new Group(
+            new Action(ROTATE_TO_POINT, "siluna", CURRENT_TAP, config.speed.rotation),
+            new Action(START_MOVING_TO_POINT, "siluna", CURRENT_TAP, config.speed.movement, config.speed.lerp),
+            new Action(ANIMATE, "siluna", "swimming")
+        ),
+        [TOUCH_MOVE_ON_SCREEN]: new Action(CHANGE_DIRECTION, CURRENT_TAP),
+        [TOUCH_END_ON_SCREEN]: new Group(
+            new Action(STOP_MOVING_TO_POINT, "siluna"),
+            new Action(ANIMATE, "siluna", "treading-water"),
+            new Action(ROTATE_TO_VECTOR, "siluna", new Vector(0, -1), config.speed.rotation / 3)
         )
-        //[TOUCH_START_ON_SCREEN]: new Sequence(
-        //    new Group(
-        //        new Action(DEBUG, "touch start on screen event triggered", CURRENT_TAP),
-        //        new Action(START_MOVING_TO_POINT, "siluna", CURRENT_TAP, config.speed.movement, config.speed.lerp),
-        //        new Action(ANIMATE, "siluna", "swimming"),
-        //        new Action(DEBUG, "touch start on screen sequence done")
-        //    ),
-        //    new Group(
-        //        new Action(DEBUG, "siluna is done moving"),
-        //        new Action(ANIMATE, "siluna", "treading-water")
-        //    )
-        //),
-        //[TOUCH_MOVE_ON_SCREEN]: new Action(DEBUG, "touch move on screen event triggered", CURRENT_TAP),
-        //[TOUCH_END_ON_SCREEN]: new Group(
-        //    new Action(DEBUG, "touch end on screen event triggered", CURRENT_TAP),
-        //    new Action(STOP_MOVING_TO_POINT, "siluna")
-        //)
     },
     entities: new Entities(
         new Entity("siluna",
